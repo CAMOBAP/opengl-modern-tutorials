6a27c35d328ee6e9a8a33a1ad7b1d33dc03895c7
Sylvain Beucler <beuc@beuc.net>
2011-09-25 11:51:20 +0200

    Average normals

--- obj/suzanne.cpp
+++ obj/suzanne.cpp
@@ -132,6 +132,7 @@ GLint create_shader(const char* filename, GLenum type)
 void load_obj(const char* filename, struct mesh* mesh) {
   ifstream in(filename, ios::in);
   if (!in) { cerr << "Cannot open " << filename << endl; exit(1); }
+  vector<int> nb_seen;
 
   string line;
   while (getline(in, line)) {
@@ -151,6 +152,7 @@ void load_obj(const char* filename, struct mesh* mesh) {
   }
 
   mesh->normals.resize(mesh->vertices.size(), glm::vec3(0.0, 0.0, 0.0));
+  nb_seen.resize(mesh->vertices.size(), 0);
   for (int i = 0; i < mesh->elements.size(); i+=3) {
     GLushort ia = mesh->elements[i];
     GLushort ib = mesh->elements[i+1];
@@ -158,7 +160,22 @@ void load_obj(const char* filename, struct mesh* mesh) {
     glm::vec3 normal = glm::normalize(glm::cross(
       glm::vec3(mesh->vertices[ib]) - glm::vec3(mesh->vertices[ia]),
       glm::vec3(mesh->vertices[ic]) - glm::vec3(mesh->vertices[ia])));
-    mesh->normals[ia] = mesh->normals[ib] = mesh->normals[ic] = normal;
+
+    int v[3];  v[0] = ia;  v[1] = ib;  v[2] = ic;
+    for (int j = 0; j < 3; j++) {
+      GLushort cur_v = v[j];
+      nb_seen[cur_v]++;
+      if (nb_seen[cur_v] == 1) {
+	mesh->normals[cur_v] = normal;
+      } else {
+	// average
+	mesh->normals[cur_v].x = mesh->normals[cur_v].x * (1.0 - 1.0/nb_seen[cur_v]) + normal.x * 1.0/nb_seen[cur_v];
+	mesh->normals[cur_v].y = mesh->normals[cur_v].y * (1.0 - 1.0/nb_seen[cur_v]) + normal.y * 1.0/nb_seen[cur_v];
+	mesh->normals[cur_v].z = mesh->normals[cur_v].z * (1.0 - 1.0/nb_seen[cur_v]) + normal.z * 1.0/nb_seen[cur_v];
+	mesh->normals[cur_v] = glm::normalize(mesh->normals[cur_v]);
+      }
+      cout << glm::length(mesh->normals[cur_v]) << endl;
+    }
   }
 }
 
