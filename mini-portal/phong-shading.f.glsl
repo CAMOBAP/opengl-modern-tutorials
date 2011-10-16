c6f3ef281ae6ce9e4e416e24a8c9f7abbbed5ecc
Sylvain Beucler <beuc@beuc.net>
2011-10-16 12:26:34 +0200

    Make a specialized, faster phong shader for the mini-portal demo

--- mini-portal/phong-shading.f.glsl
+++ mini-portal/phong-shading.f.glsl
@@ -13,27 +13,17 @@ struct lightSource
   vec4 position;
   vec4 diffuse;
   vec4 specular;
-  float constantAttenuation, linearAttenuation, quadraticAttenuation;
+  float invLinearAttenuation;
   float spotCutoff, spotExponent;
   vec3 spotDirection;
 };
-const int numberOfLights = 2;
-lightSource[numberOfLights] lights;
 lightSource light0 = lightSource(
   vec4(0.0,  3.0,  0.0, 1.0),
   vec4(1.0,  1.0,  1.0, 1.0),
   vec4(1.0,  1.0,  1.0, 1.0),
-  0.0, 1.0, 0.0,
+  2.0,
   180.0, 0.0,
   vec3(0.0, 0.0, 0.0)
-);
-lightSource light1 = lightSource(
-    vec4(0.0, -2.0,  0.0, 1.0),
-    vec4(2.0,  0.0,  0.0, 1.0),
-    vec4(0.1,  0.1,  0.1, 1.0),
-    0.0, 1.0, 0.0,
-    80.0, 10.0,
-    vec3(0.0, 1.0, 0.0)
 );
 vec4 scene_ambient = vec4(0.2, 0.2, 0.2, 1.0);
 
@@ -53,9 +43,6 @@ material frontMaterial = material(
 
 void main()
 {
-  lights[0] = light0;
-  lights[1] = light1;
-
   vec3 normalDirection = normalize(varyingNormalDirection);
   vec3 viewDirection = normalize(vec3(v_inv * vec4(0.0, 0.0, 0.0, 1.0) - position));
   vec3 lightDirection;
@@ -64,38 +51,13 @@ void main()
   // initialize total lighting with ambient lighting
   vec3 totalLighting = vec3(scene_ambient) * vec3(frontMaterial.ambient);
   
-  for (int index = 0; index < numberOfLights; index++) // for all light sources
-    {
-      if (0.0 == lights[index].position.w) // directional light?
-	{
-	  attenuation = 1.0; // no attenuation
-	  lightDirection = normalize(vec3(lights[index].position));
-	} 
-      else // point light or spotlight (or other kind of light) 
-	{
-	  vec3 positionToLightSource = vec3(lights[index].position - position);
-	  float distance = length(positionToLightSource);
-	  lightDirection = normalize(positionToLightSource);
-	  attenuation = 1.0 / (lights[index].constantAttenuation
-			       + lights[index].linearAttenuation * distance
-			       + lights[index].quadraticAttenuation * distance * distance);
-	  
-	  if (lights[index].spotCutoff <= 90.0) // spotlight?
-	    {
-	      float clampedCosine = max(0.0, dot(-lightDirection, normalize(lights[index].spotDirection)));
-	      if (clampedCosine < cos(radians(lights[index].spotCutoff))) // outside of spotlight cone?
-		{
-		  attenuation = 0.0;
-		}
-	      else
-		{
-		  attenuation = attenuation * pow(clampedCosine, lights[index].spotExponent);   
-		}
-	    }
-	}
+	  vec3 positionToLightSource = vec3(light0.position - position);
+	  float distanceSqr = dot(positionToLightSource, positionToLightSource);
+	  lightDirection = inversesqrt(distanceSqr) * positionToLightSource;
+	  attenuation = light0.invLinearAttenuation * inversesqrt(distanceSqr);
       
       vec3 diffuseReflection = attenuation 
-	* vec3(lights[index].diffuse) * vec3(frontMaterial.diffuse)
+	* vec3(light0.diffuse) * vec3(frontMaterial.diffuse)
 	* max(0.0, dot(normalDirection, lightDirection));
       
       vec3 specularReflection;
@@ -105,12 +67,11 @@ void main()
 	}
       else // light source on the right side
 	{
-	  specularReflection = attenuation * vec3(lights[index].specular) * vec3(frontMaterial.specular)
+	  specularReflection = attenuation * vec3(light0.specular) * vec3(frontMaterial.specular)
 	    * pow(max(0.0, dot(reflect(-lightDirection, normalDirection), viewDirection)), frontMaterial.shininess);
 	}
 
       totalLighting = totalLighting + diffuseReflection + specularReflection;
-    }
   
   gl_FragColor = vec4(totalLighting, 1.0);
 }
