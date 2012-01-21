a63c6ef5fcba389d0c050d1606f97bf229f8afe6
Sylvain Beucler <beuc@beuc.net>
2012-01-21 15:17:25 +0100

    NDK debugging now works

--- tut01_intro-android/jni/Android.mk
+++ tut01_intro-android/jni/Android.mk
@@ -19,6 +19,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := native-activity
 LOCAL_SRC_FILES := main.c GL/glew.c tut.cpp
 LOCAL_CPPFLAGS  := -I/usr/src/glm
+LOCAL_CXXFLAGS  := -gstabs+
 LOCAL_LDLIBS    := -llog -landroid -lEGL -lGLESv2
 LOCAL_STATIC_LIBRARIES := android_native_app_glue
 
