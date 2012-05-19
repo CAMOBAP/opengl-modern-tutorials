43bb7593aae5ad84e9616c138543a89a5f5df918
Sylvain Beucler <beuc@beuc.net>
2012-05-19 15:53:43 +0200

    typo

--- textured_spheres/sphere.cpp
+++ textured_spheres/sphere.cpp
@@ -15,7 +15,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-#include "SOIL/SOIL.h"
+#include <SOIL/SOIL.h>
 #include "../common/shader_utils.h"
 
 #include <iostream>
