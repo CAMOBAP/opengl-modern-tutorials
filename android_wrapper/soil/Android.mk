48fcd5bca06a020ee889001b413884bec9af9d12
Sylvain Beucler <beuc@beuc.net>
2012-05-19 14:36:36 +0200

    Android: Add SOIL module

---
+++ android_wrapper/soil/Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := soil
+LOCAL_SRC_FILES := lib/libSOIL.a
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+
+include $(PREBUILT_STATIC_LIBRARY)
