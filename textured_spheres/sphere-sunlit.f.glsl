fa1dc2d2282ead680e752696d6e6f06a5d0b4c83
Sylvain Beucler <beuc@beuc.net>
2012-05-29 09:08:30 +0200

    Adapt light position

--- textured_spheres/sphere-sunlit.f.glsl
+++ textured_spheres/sphere-sunlit.f.glsl
@@ -7,9 +7,9 @@ void main(void)
 {
     vec2 longitudeLatitude = vec2((atan(texCoords.y, texCoords.x) / 3.1415926 + 1.0) * 0.5,
                                   (asin(texCoords.z) / 3.1415926 + 0.5));
- 
+
     vec4 nighttimeColor = texture2D(mytexture, longitudeLatitude);
-    vec4 daytimeColor = texture2D(mytexture_sunlit, longitudeLatitude); 
+    vec4 daytimeColor = texture2D(mytexture_sunlit, longitudeLatitude);
 
     //gl_FragColor = mix(nighttimeColor, daytimeColor, levelOfLighting);
     gl_FragColor = mix(nighttimeColor, daytimeColor, levelOfLighting);
