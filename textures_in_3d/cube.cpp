8679b4d4f9c53b196693d9e952394b61360e5ebe
Sylvain Beucler <beuc@beuc.net>
2013-06-02 20:48:25 +0200

    typo

--- textures_in_3d/cube.cpp
+++ textures_in_3d/cube.cpp
@@ -405,7 +405,7 @@ void free_resources()
 }
 
 void logic() {
-  float angle = glutGet(GLUT_ELAPSED_TIME) / 1000.0 * 10;  // 30° per second
+  float angle = glutGet(GLUT_ELAPSED_TIME) / 1000.0 * 10;  // 10° per second
   glm::mat4 anim = \
     glm::rotate(glm::mat4(1.0f), angle*3.0f, glm::vec3(1, 0, 0)) *  // X axis
     glm::rotate(glm::mat4(1.0f), angle*2.0f, glm::vec3(0, 1, 0)) *  // Y axis
